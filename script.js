761d938f6a28bc985c6d7d2cbfcd3c792a9468a0
Erica Giovani <ericagiovani99@gmail.com>
2026-08-09 18:23:41 +0700

    create query selector for todo app (js)

--- script.js
+++ script.js
@@ -13,4 +13,16 @@ class TodoList{
   constructor(){
     this.todos = [];
   }
+  addTodo(todo){
+    this.todos.push(todo);
+  }
+  deleteTodo(index){
+    this.todos.splice(index,1);
+  }
 }
+
+const titleInput = document.querySelector('#title');
+const dateInput = document.querySelector('#date');
+const descriptionInput = document.querySelector('#description');
+const timeInput = document.querySelector('#time');
+const buttonInput = document.querySelector('#add-activities');
